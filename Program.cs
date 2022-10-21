d632ee6c706fe8f014041b5ebcd13026208d8934
igor90935 <115576253+igor90935@users.noreply.github.com>
2022-10-21 13:13:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 10           
+
+
+Console.Clear();
+Console.WriteLine("Задача 10 ");
+Console.Write("Введи трёхзначное число: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+//
+
+//Задача 13
+
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
+//
+
+//Задача 15
+
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) 
+{
+  if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(этот день выходной) -> да");
+  }
+  else Console.WriteLine("(этот день не выходной) -> нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+//
+
+
